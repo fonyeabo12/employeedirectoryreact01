c4547e821187c422e0971d058dd37ee0b44ad2ed
FrankBlack <fonyeabo12@gmail.com>
2020-09-27 21:01:06 -0500

    Updates

---
+++ precache-manifest.fdddb8a9108985ae64fb65d514eb7837.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "122fcca512eee089ed09ecbd89706004",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ed49fcc0787544e04fd",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "8445eec92d36fbd03834",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "5ed49fcc0787544e04fd",
+    "url": "/static/js/2.15abba21.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.15abba21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8445eec92d36fbd03834",
+    "url": "/static/js/main.18425926.chunk.js"
+  },
+  {
+    "revision": "d86234b6413ceb92eb0c",
+    "url": "/static/js/runtime-main.b7999843.js"
+  }
+]);
